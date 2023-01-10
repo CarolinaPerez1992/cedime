de63ef3070ad6a0667e8dad5f673f31771c1595b
maurito.harretche <mauroharretche@gmail.com>
2023-01-10 13:44:13 -0300

    update-hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Header from "../Header/Header";
 import "./Hero.css";
+import hero_image from "../../assets/hero_image.png";
+import hero_image_back from "../../assets/hero_image_back.png";
+import heart from "../../assets/heart.png";
+import calories from "../../assets/calories.png";
 
 const Hero = () => {
   return (
@@ -46,7 +50,14 @@ const Hero = () => {
           <buttons className="btn">Lorem, ipsum.</buttons>
         </div>
       </div>
-      <div className="right-h">Right</div>
+      <div className="right-h">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis tempora aliquam officiis. Repellat eius fugit excepturi 
+      <div className="heart">
+        <img src={heart} alt="corazon" />
+        <span>heart</span><span> heart</span>
+      </div>
+      <img src={hero_image} alt="hero_image" className="hero-image"/>
+      <img src={hero_image_back} alt="hero_image_back" className="hero-image_back"/>
+      </div>
     </div>
   );
 };
