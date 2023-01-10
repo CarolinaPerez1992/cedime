fc196f137d1409d3aa13eae9aad98c5a8f0da1d4
maurito.harretche <mauroharretche@gmail.com>
2023-01-10 12:11:32 -0300

    update

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -13,11 +13,37 @@ const Hero = () => {
         </div>
         <div className="hero-text">
           <div>
-            <span className="stroke-text">Lorem </span>
+            <span className="stroke-text">Lorem</span>
             <span>ipsum</span>
           </div>
-          <div><span>
-            Lorem, ipsum.</span></div>
+          <div>
+            <span>Lorem, ipsum.</span>
+          </div>
+          <div>
+            <span className="hero-text-span">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque,
+              consequatur!
+            </span>
+          </div>
+        </div>
+        <div className="figures">
+          <div>
+            <span>+17</span>
+            <span>Profesionales</span>
+          </div>
+          <div>
+            <span>+30</span>
+            <span>Colaboradores</span>
+          </div>
+          <div>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+        {/* hero buttons */}
+        <div className="hero-buttons">
+          <buttons className="btn">Lorem, ipsum.</buttons>
+          <buttons className="btn">Lorem, ipsum.</buttons>
         </div>
       </div>
       <div className="right-h">Right</div>
