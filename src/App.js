cb325546977474b9dce0a2cb7f52e895cd112c80
maurito.harretche <mauroharretche@gmail.com>
2023-01-12 10:43:06 -0300

    update

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import './App.css';
 import Hero from './components/Hero/Hero.jsx'
 import Reasons from './components/Reasons/Reasons.jsx';
 import Plans from './components/Plans/Plans.jsx';
+import Programs from './components/Programs/Programs.jsx'
 function App() {
   return (
     <div className="App">
       <Hero/>
-
-
-
-
+      <Programs/>
       <Reasons/>
       <Plans/>
-
-
     </div>
   );
 }
